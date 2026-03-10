1b73a69f0f37ca595f5e2a1fe386f0a1c2155804
Hariharan Ramesh <hramesh@gh-login01.delta.ncsa.illinois.edu>
2026-03-10 17:45:57 -0500

    Update dataset

--- run_wiz.sh
+++ run_wiz.sh
@@ -9,24 +9,29 @@ exec > "$LOGFILE" 2>&1
 source florist/bin/activate
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-# python3 main.py --global_model 'huggyllama/llama-7b' --data_path  "./data_wiz" --output_dir './llama7b-wizard-homo-75-1-100-flex/' --num_communication_rounds 75 --local_num_epochs 1 --method flex --num_clients 100 --local_micro_batch_size 8  --local_batch_size 64
+# Download TinyLlama model if not already present
+if [ ! -f "tinyllama/model.safetensors" ]; then
+    echo "Downloading TinyLlama model..."
+    mkdir -p tinyllama
+    cd tinyllama
+    wget -O tinyllama/model.safetensors \
+        "https://huggingface.co/TinyLlama/TinyLlama-1.1B-Chat-v1.0/resolve/main/model.safetensors"
+    cd ../
+fi
 
-# python3 main.py --global_model 'huggyllama/llama-7b' --data_path  "./data_alpaca" --output_dir './llama7b-alpaca-homo-75-1-100-flex/' --num_communication_rounds 75 --local_num_epochs 1 --method flex --num_clients 100 --local_micro_batch_size 4  --local_batch_size 64
+python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=$((RANDOM%10000+20000)) --use_env main.py --global_model 'tinyllama' --data_path  "./data_alpaca" --output_dir './tinyllama-alpaca-homo-75-1-100-florist/' --num_communication_rounds 75 --local_num_epochs 1 --method florist --num_clients 100
 
-# python3 main.py --global_model 'huggyllama/llama-7b' --data_path  "./data" --output_dir './llama7b-dolly-homo-75-1-100-flex/' --num_communication_rounds 75 --local_num_epochs 1 --method flex --num_clients 100 --local_micro_batch_size 4  --local_batch_size 64
+python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=$((RANDOM%10000+20000)) --use_env main.py --global_model 'tinyllama' --data_path  "./data_alpaca" --output_dir './tinyllama-alapca-homo-75-1-100-flora/' --num_communication_rounds 75 --local_num_epochs 1 --method flora --num_clients 100
 
+
+# python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=$((RANDOM%10000+20000)) --use_env main.py --global_model 'meta-llama/Llama-3.2-1B' --data_path  "./data_alpaca" --output_dir './Llama-3.2-1B-alpaca-homo-75-1-100-florist/' --num_communication_rounds 75 --local_num_epochs 1 --method florist --num_clients 100
+
+# python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=$((RANDOM%10000+20000)) --use_env main.py --global_model 'meta-llama/Llama-3.2-1B' --data_path  "./data_alpaca" --output_dir './Llama-3.2-1B-alapca-homo-75-1-100-flora/' --num_communication_rounds 75 --local_num_epochs 1 --method flora --num_clients 100
+=
 # python3 main.py --global_model 'meta-llama/Llama-3.2-1B' --data_path  "./data_alpaca" --output_dir './Llama-3.2-1B-alpaca-heter-75-1-100-ffa/' --num_communication_rounds 75 --local_num_epochs 1 --method ffa --num_clients 100 --heter True
 
 # python3 main.py --global_model 'tinyllama' --data_path  "./data_wiz" --output_dir './tinyllama-wizard-homo-75-1-100-florist/' --num_communication_rounds 75 --local_num_epochs 1 --method florist --num_clients 100 --threshold 0.90
 
 # python3 main.py --global_model 'tinyllama' --data_path  "./data_wiz" --output_dir './tinyllama-wizard-heter-75-1-100-florist/' --num_communication_rounds 75 --local_num_epochs 1 --method florist --num_clients 100 --threshold 0.90 --heter True
 
-# python3 main.py --global_model 'huggyllama/llama-7b' --data_path  "./data" --output_dir './llama7b-dolly-heter-75-1-100-ffa/' --num_communication_rounds 75 --local_num_epochs 1 --method ffa --num_clients 100 --local_micro_batch_size 4  --local_batch_size 64 --heter True --zero_padding True
-
-# python3 main.py --global_model 'huggyllama/llama-7b' --data_path  "./data" --output_dir './llama7b-dolly-heter-75-1-100-fedit/' --num_communication_rounds 75 --local_num_epochs 1 --method fedit --num_clients 100 --local_micro_batch_size 4  --local_batch_size 64 --heter True --zero_padding True
-
-
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=$((RANDOM%10000+20000)) --use_env main.py --global_model 'meta-llama/Llama-3.2-1B' --data_path  "./data_alpaca" --output_dir './Llama-3.2-1B-alpaca-homo-75-1-100-florist/' --num_communication_rounds 75 --local_num_epochs 1 --method florist --num_clients 100
-
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=$((RANDOM%10000+20000)) --use_env main.py --global_model 'meta-llama/Llama-3.2-1B' --data_path  "./data_alpaca" --output_dir './Llama-3.2-1B-alapca-homo-75-1-100-flora/' --num_communication_rounds 75 --local_num_epochs 1 --method flora --num_clients 100
-
+# python3 main.py --global_model 'tinyllama' --data  "./data_wiz" --output_dir './tinyllama-dolly-heter-75-1-100-fedit/' --num_communication_rounds 75 --local_num_epochs 1 --method fedit --num_clients 100 --threshold 0.90 --heter True --zero_padding True
